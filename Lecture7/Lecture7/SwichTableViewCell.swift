565d505868711be328f3c6d5696b669f4eea2c65
Slavuta75 <slavuta75@gmail.com>
2018-05-25 14:24:04 +0300

    Dynamic Table without switch

---
+++ Lecture7/Lecture7/SwichTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  swichTableViewCell.swift
+//  Lecture7
+//
+//  Created by Slava Utyuzh on 25.05.2018.
+//  Copyright © 2018 Slava Utyuzh. All rights reserved.
+//
+
+import UIKit
+
+class SwichTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Lecture7/Lecture7/ViewController.swift
+++ Lecture7/Lecture7/ViewController.swift
@@ -8,12 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+ 
+    @IBOutlet weak var tableView: UITableView!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+//    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+//        print(#function)
+//        return 2
+//    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+          print(#function)
+        return 2
+    }
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       print(#function)
+        if  section == 0 {
+            return 3
+        }
+        
+        return 2
+    }
+    
+  
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+   print(#function)
+        // для отображение всех ячеек
+        //     let cell = UITableViewCell()
+        
+        // для отображения в памяти только нескольких ячеек
+ //       let cell = tableView.dequeueReusableCell(withIdentifier: "cellDetail", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: (indexPath.section == 0 ? "cellDetail" : "cell"), for: indexPath)
+        if indexPath.section == 0 {
+            cell.textLabel?.text = "Slava"
+        cell.textLabel?.textColor = UIColor.blue
+        cell.detailTextLabel?.text = "\(indexPath.row)"
+        cell.imageView?.image = UIImage(named: "key")
+        } else {
+            cell.textLabel?.text = "Utyuzh"
+            cell.backgroundColor = UIColor.cyan
+        }
+//        let cell1 = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell1.textLabel?.text = "Utyuzh"
+//        cell1.backgroundColor = UIColor.gray
+        return cell
+    }
+    
+  
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
